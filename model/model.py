277e54e7573f263abb944e00ad2afd7e71cbb82f
karachunYaraslau <karacuna396@gmail.com>
2024-10-31 19:43:04 +0300

    delete not valuable saved_model_1

--- model/model.py
+++ model/model.py
@@ -6,10 +6,13 @@ from scripts.preprocess_data import get_data_from_file
 
 model = AutoModelForTokenClassification.from_pretrained("./model/saved_model_2")
 tokenizer = AutoTokenizer.from_pretrained("./model/saved_model_2")
+ner_pipeline = pipeline("ner", model=model, tokenizer=tokenizer)
+results = ner_pipeline("I buy a new table from Ikea")
+for it in results:
+    print(it)
 
-
-def calculations(data : str):
-    ner_pipeline = pipeline("ner", model=model, tokenizer=tokenizer)
-    results = ner_pipeline(data)
-    for it in results:
-        print(it)
+# def calculations(data : str):
+#     ner_pipeline = pipeline("ner", model=model, tokenizer=tokenizer)
+#     results = ner_pipeline(data)
+#     for it in results:
+#         print(it)
